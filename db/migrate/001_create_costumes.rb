1b00478700f59062e161b20851d2a18487093c37
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 00:32:59 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,8 +6,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.float :price
       t.integer :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps
     end
   end
 end
