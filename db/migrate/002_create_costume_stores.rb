e956c64451a7f1bc25eb3d6a63143a4b1244c267
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 00:17:48 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
+class CreateCostumeStors < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.string :in_business
+      t.string :opening_time
+      t.string :closing_time
+
+    end
+  end
+end
+
 # Create your costume_stores migration here
