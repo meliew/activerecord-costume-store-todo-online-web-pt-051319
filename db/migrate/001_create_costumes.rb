cf5ce04f1fe139ffd46f4aef1aa3c93ba94f6402
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-28 00:20:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,11 +3,11 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.integer :size
       t.string :image_url
-      t.timestamp :created_at
-      t.timestamp :updated_at 
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
